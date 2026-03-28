84205f3174cf6106c9a46a4548e0815466cda03e
DelijaAnte <antedelija77@gmail.com>
2026-03-28 17:52:24 +0100

    fixed metro config, restore default Metro resolver fields for RN codegen

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,6 @@
-// Force Metro to prefer CommonJS entry points for certain packages.
-// This avoids issues where Metro picks an ES-module build that can't be resolved correctly.
 const { getDefaultConfig } = require("@expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
-
-config.resolver.resolverMainFields = ["main", "module", "react-native"];
 
 module.exports = config;
 
